39b6b549e12f8fc6bf462d69170dabcc9014eef8
David52PG <david020522@gmail.com>
2024-01-28 00:55:08 +0100

    cerrando_bucle

--- main.py
+++ main.py
@@ -10,16 +10,60 @@ def buscapal():
         print("No encontrado")
         buscapal()
 
+def lookpath(padre, objetivo, opciones = [], actual = []):
+    padre = int(padre)
+    objetivo = int(objetivo)
+
+    if padre > objetivo:
+        for pal in pals:
+            nombre, numero = pal
+            numero = int(numero)
+            if numero < padre:
+                if len(actual) == 0:
+                    actual.append(numero)
+                    numero = (numero + padre)/2
+                    actual.append(numero)
+                    posible = lookpath(numero, objetivo, opciones, actual)
+                elif actual[len(actual)-2] != numero:
+                    actual.append(numero)
+                    numero = (numero + padre)/2
+                    actual.append(numero)
+                    posible = lookpath(numero, objetivo, opciones, actual)
+                    
+def searchpal():
+    
+
+    elif padre < objetivo:
+        print("hola")
+
+    else:
+        if len(opciones) == 0:
+            opciones.append(actual)
+            return opciones
+        elif len(opciones[0]) < len(actual):
+            return opciones
+        elif len(opciones[0]) == len(actual):
+            opciones.append(actual)
+            return opciones
+        else:
+            opciones.clear()
+            opciones.append(actual)
+            return opciones
+            
+            
+
+
 with open('infopals.txt', 'r') as archivo:
         pals = [nombre.strip().lower().split(' - ') for nombre in archivo.readlines()]
 
 objetivo = ""
 
 print("¿objetivo?")
-numero_objetivo = buscapal()
+numero_objetivo = 280 #buscapal()
 
 print("¿padre?")
-padre = buscapal()
+padre = 660 #buscapal()
 
-print(numero_objetivo)
-print(padre)
+lista = lookpath(padre, numero_objetivo)
+
+print(lista)
