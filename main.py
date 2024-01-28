589cb9246a50afe56e7dc4e0ac61f4acba5d7eb5
David52PG <david020522@gmail.com>
2024-01-28 19:57:25 +0100

    empezando

--- main.py
+++ main.py
@@ -1,13 +1,21 @@
-with open('infopals.txt', 'r') as archivo:
-        nombres = [nombre.strip() for nombre in archivo.readlines()]
+import json
 
-print("¿objetivo?")
-objetivo = input()
+with open("infopals.txt", "r") as file:
+    for pal in 
 
-numero = 0
-for nombre in nombres:
-    print(numero)
-    numero += 1
-    if objetivo == nombre:
-        print("Encontrado: " + nombre)
-        break
+datos = []
+
+for i in range(1, 4):
+    dato = {
+        "padre1": f"Padre1 del dato {i}",
+        "padre2": f"Padre2 del dato {i}",
+        "resultado": f"Resultado del dato {i}"
+    }
+    datos.append(dato)
+
+json_data = {
+    "datos": datos
+}
+
+json_string = json.dumps(json_data, indent=4)
+print(json_string)
