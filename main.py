15260e196e4f9b5af1ef72ae64e5e86162d57b4f
David52PG <david020522@gmail.com>
2024-01-28 20:01:08 +0100

    preparado

--- main.py
+++ main.py
@@ -1,17 +1,25 @@
 import json
 
-with open("infopals.txt", "r") as file:
-    for pal in 
+pals = []
+with open('infopals.txt', 'r') as archivo:
+        Npals = [nombre.strip().lower().split(' - ') for nombre in archivo.readlines()]
+        for nombre, poder in Npals:
+            pals.append(Pal(nombre, poder, []))
+        del Npals
 
 datos = []
 
-for i in range(1, 4):
-    dato = {
-        "padre1": f"Padre1 del dato {i}",
-        "padre2": f"Padre2 del dato {i}",
-        "resultado": f"Resultado del dato {i}"
-    }
-    datos.append(dato)
+for pal in pals:
+    for pal2 in pals:
+        print(pal.nombre, pal2.nombre)
+        print("hijo?")
+        hijo = input()
+        dato = {
+        "padre1": pal,
+        "padre2": pal2,
+        "resultado": hijo
+        }
+        datos.append(dato)
 
 json_data = {
     "datos": datos
@@ -19,3 +27,4 @@ json_data = {
 
 json_string = json.dumps(json_data, indent=4)
 print(json_string)
+
